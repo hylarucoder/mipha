b6d87b4248f0fbb42fac0cee34b2c6543913667b
twocucao <twocucao@gmail.com>
2020-05-05 18:34:38 +0800

    temp

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # mipha
 
 Yet Another Waifu For Paint for Python
+
+
+```
+rustup clean && rustup update
+rustup toolchain install nightly
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
-use qrcode::QrCode;
-use image::Luma;
+extern crate mipha;
+
+use mipha::QrCodeEcc;
+use mipha::QrCode;
+
+
+
+// Prints the given QrCode object to the console.
+fn print_qr(qr: &QrCode) {
+	let border: i32 = 4;
+	for y in -border .. qr.size() + border {
+		for x in -border .. qr.size() + border {
+			let c: char = if qr.get_module(x, y) { '█' } else { ' ' };
+			print!("{0}{0}", c);
+		}
+		println!();
+	}
+	println!();
+}
+
+
+// Converts the given borrowed string slice to a new character vector.
+fn to_chars(text: &str) -> Vec<char> {
+	text.chars().collect()
+}
 
 fn main() {
-    // Encode some data into bits.
-    let code = QrCode::new(b"01234567").unwrap();
-
-    // Render the bits into an image.
-    let image = code.render::<Luma<u8>>().build();
-
-    // Save the image.
-    image.save("/tmp/qrcode.png").unwrap();
+    let text: &'static str = "Hello, world!";   // User-supplied Unicode text
+	let errcorlvl: QrCodeEcc = QrCodeEcc::Low;  // Error correction level
+	
+	// Make and print the QR Code symbol
+	let qr: QrCode = QrCode::encode_text(text, errcorlvl).unwrap();
+	print_qr(&qr);
+	println!("{}", qr.to_svg_string(4));
 }
